317b2dc5cd448c80abcd4255933e28a4ad691a97
OganM <ogan.mancarci@gmail.com>
2020-07-03 01:41:37 -0700

    api

--- R/api.R
+++ R/api.R
@@ -20,6 +20,8 @@ getContent = function(url){
     return(content)
 }
 
+
+
 rating_history = function(game = 'aoe2de', 
                           leaderboard_id = 3,
                           start = 0,
@@ -37,3 +39,70 @@ rating_history = function(game = 'aoe2de',
     getContent(url)
 }
 
+
+nullableString = function(...){
+    args = list(...)
+    
+    if(is.null(args[[1]])){
+        out = ''
+    } else{
+        out = glue::glue('{names(args[1])}={args[[1]]}')
+    }
+    return(out)
+    
+}
+
+leaderboard = function(game= 'aoe2de',
+                       leaderboard_id = 3,
+                       start = 1,
+                       count = 10000,
+                       search = NULL,
+                       steam_id = NULL,
+                       profile_id = NULL){
+    
+    search = nullableString(search = search)
+    steam_id = nullableString(steam_id = steam_id)
+    profile_id = nullableString(profile_id = profile_id)
+    
+    url = glue::glue('{aoe2netBase()}leaderboard?game={game}&leaderboard_id={leaderboard_id}&start={start}&count={count}&{search}&{steam_id}&{profile_id}')
+    getContent(url)
+    
+}
+
+
+lobbies = function(game = 'aoe2de'){
+    url = glue::glue('{aoe2netBase()}lobbies?game={game}')
+    getContent(url)
+}
+
+last_match = function(game = 'aoe2de',
+                      steam_id = NULL,
+                      profile_id = NULL){
+    
+    if(is.null(steam_id)){
+        id = glue::glue("profile_id={profile_id}")
+    }else{
+        id = glue::glue('steam_id={steam_id}')
+    }
+    
+    url = glue::glue('{aoe2netBase()}player/lastmatch?game={game}&{id}')
+    getContent(url)
+    
+}
+
+
+match_history = function(game = 'aoe2de',
+                         start = 0,
+                         count = 1000,
+                         steam_id = NULL,
+                         profile_id = NULL){
+    if(is.null(steam_id)){
+        id = glue::glue("profile_id={profile_id}")
+    }else{
+        id = glue::glue('steam_id={steam_id}')
+    }
+    
+    url = glue::glue('{aoe2netBase()}player/matches?game={game}&start={start}&count={count}&{id}')
+    getContent(url)
+    
+}
--- test.R
+++ test.R
@@ -1,3 +1,14 @@
+library(dplyr)
+library(ggplot2)
 devtools::load_all()
 steam_id = "76561198024069852"
-rating_history(steam_id = steam_id)
+rating_history(steam_id = steam_id) %>% purrr::map_int('rating') %>% rev %>% data.frame(rating = ., game = 1:length(.)) %>% 
+    ggplot(aes(x = game, y = rating)) +
+    geom_line(linetype = 'dashed') + 
+    geom_point() + 
+    cowplot::theme_cowplot()
+
+
+last_match(steam_id = steam_id) -> hede
+
+history = match_history(steam_id = steam_id)
